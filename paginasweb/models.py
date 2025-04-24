826b12efdfeac70b733294e08246c8705158159a
unknown <ana.trajano05@gmail.com>
2025-04-24 11:25:10 -0300

    Criação de classes e atributos

--- paginasweb/models.py
+++ paginasweb/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Campus(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=150)
+    campus = models.ForeignKey(Campus,on_delete=models.PROTECT)
+
+ #class TipoSolicitação(models.Model):
+
+    descricao : models.CharField(max_length=250)
+    prazo_externo :models.CharField(max_length=250)
+    prazo_externo_dias :models.PositiveSmallIntegerField(default=0)
+    prazo_interno :models.CharField(max_length=250)
+    prazo_interno_dias :models.PositiveSmallIntegerField(default=0)
+
+
+
+
