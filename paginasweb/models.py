25dc8222146969423f218c6982eff3702bbaae78
unknown <ana.trajano05@gmail.com>
2025-05-08 10:56:36 -0300

    Criação das views, urls e tamplate para inserir registro

--- paginasweb/models.py
+++ paginasweb/models.py
@@ -3,15 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Campus(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100) 
+    cadastro_em = models.DateTimeField(auto_now_add=True)
 
 class Curso(models.Model):
     nome = models.CharField(max_length=150)
     campus = models.ForeignKey(Campus,on_delete=models.PROTECT)
+    cadastro_em = models.DateTimeField(auto_now_add=True)
 
- #class TipoSolicitação(models.Model):
+class TipoSolicitação(models.Model):
 
-    descricao : models.CharField(max_length=250)
+    descricao : models.CharField(max_length=250, verbose_name="descrição") # type: ignore
     prazo_externo :models.CharField(max_length=250)
     prazo_externo_dias :models.PositiveSmallIntegerField(default=0)
     prazo_interno :models.CharField(max_length=250)
--- paginasweb/views.py
+++ paginasweb/views.py
@@ -1,9 +1,27 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Campus, Curso
+from django.urls import reverse_lazy
 
 
-class IndexView(TemplateView):
-    template_name = 'paginasweb/index.html'
+class Inicio(TemplateView):
+    template_name = 'paginas/index.html'
 
 
 class SobreView(TemplateView):
-    template_name = 'paginasweb/sobre.html'
+    template_name = 'paginas/sobre.html'
+
+
+class CampusCreate(CreateView):
+    template_name = "paginas/form.html"  # arquivo html com o <form>.
+    model = Campus  # classe criada no models.
+    fields = ['nome']  # lista com os nomes dos atributos.
+    success_url = reverse_lazy('index')  # name da url para redirecionar.
+    extra_context = {}
+
+
+class CursoCreate(CreateView):
+    template_name = 'paginasweb/form.html'
+    model = Curso
+    fields = ['nome', 'campus']
+    success_url = reverse_lazy('index')
