32440962eac4542624cfe8c258c9cad1855fe043
unknown <ana.trajano05@gmail.com>
2025-07-24 11:44:30 -0300

    Controle de acesso às views com LoginRequiredMixin, Adição do Framework de Mensagens com SuccessMessageMixin

--- paginasweb/views.py
+++ paginasweb/views.py
@@ -5,6 +5,20 @@ from django.urls import reverse_lazy
 from .models import Doador, Instituicao, Status, Doacao, Historia_Inspiradoras
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+
+class doadorList(LoginRequiredMixin, ListView):
+    model = Doador
+    template_name = 'doador.html'
+
+class doacaoList(LoginRequiredMixin, ListView):
+    model = Doação
+    template_name = 'doacao.html'
 
 
 class PaginaInicial(TemplateView):
@@ -13,109 +27,111 @@ class PaginaInicial(TemplateView):
 class Sobre(TemplateView):
     template_name = 'paginasweb/sobre.html'
 
-class DoadorCreate(LoginRequiredMixin, CreateView):
+class DoadorCreate(SuccessMessageMixin,LoginRequiredMixin, CreateView):
     template_name = 'paginasweb/form.html'
     model = Doador
     fields = ['nome', 'telefone', 'cidade']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar doador", "botao" : "Cadastrar"}
+    success_message = "Doador criado com sucesso!"
 
-class InstituicaoCreate(LoginRequiredMixin, CreateView):
+class InstituicaoCreate(SuccessMessageMixin,LoginRequiredMixin, CreateView):
     template_name = 'paginasweb/form.html'
     model = Instituicao
     fields = ['nome', 'telefone', 'cidade', 'tipo', 'descricao']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Instituicao", "botao" : "Cadastrar"}
-
-class StatusCreate(LoginRequiredMixin, CreateView):
+    success_message = "Instituição criada com sucesso!"
+class StatusCreate(SuccessMessageMixin,LoginRequiredMixin, CreateView):
     template_name = 'paginasweb/form.html'
     model = Status
     fields = ['nome','pode_editar']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Status", "botao" : "Cadastrar"}
-
-class DoacaoCreate(LoginRequiredMixin, CreateView):
+    
+class DoacaoCreate(SuccessMessageMixin,LoginRequiredMixin, CreateView):
     template_name = 'paginasweb/form.html'
     model = Doacao
     fields = ['tipo', 'quantidade', 'data', 'doador', 'instituicao', 'status']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Doacao", "botao" : "Cadastrar"}
-
-class Historia_InspiradorasCreate(LoginRequiredMixin, CreateView):
+    success_message = "Doação criada!"
+class Historia_InspiradorasCreate(SuccessMessageMixin,LoginRequiredMixin, CreateView):
     model = Historia_Inspiradoras
     fields = ['titulo', 'conteudo', 'autor', 'doador', 'instituicao']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Historia Inspiradoras", "botao" : "Cadastrar"}
-
+    success_message = "Historia Inspiradora criada com sucesso!"
 
 #############################################################################################################
 
-class DoadorUpdate(UpdateView):
+class DoadorUpdate(SuccessMessageMixin,UpdateView):
     template_name = 'paginasweb/form.html'
     model = Doador
     fields = ['nome', 'telefone', 'cidade']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Doador", "botao" : "Editar"}
+    success_message = "Doador atualizado!"
 
-class InstituicaoUpdate(UpdateView):
+class InstituicaoUpdate(SuccessMessageMixin,UpdateView):
     template_name = 'paginasweb/form.html'
     model = Instituicao
     fields = ['nome', 'telefone', 'cidade', 'tipo', 'descricao']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Instituicao", "botao" : "Editar"}
-
-class StatusUpdate(UpdateView):
+    success_message = "Instituição atualizada!"
+class StatusUpdate(SuccessMessageMixin,UpdateView):
     template_name = 'paginasweb/form.html'
     model = Status
     fields = ['nome','pode_editar']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Status", "botao" : "Editar"}
-
-class DoacaoUpdate(UpdateView):
+    success_message = "Status atualizado!"
+class DoacaoUpdate(SuccessMessageMixin,UpdateView):
     template_name = 'paginasweb/form.html'
     model = Doacao
     fields = ['tipo', 'quantidade', 'data', 'doador', 'instituicao', 'status']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Doacao", "botao" : "Editar"}
-
-class Historia_InspiradorasUpdate(UpdateView):
+    success_message = "Doação atualizada!"
+class Historia_InspiradorasUpdate(SuccessMessageMixin,UpdateView):
     model = Historia_Inspiradoras
     fields = ['titulo', 'conteudo', 'autor', 'doador', 'instituicao']
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Cadastrar Historia_Inspiradoras", "botao" : "Editar"}
-
+    success_message = "Historia Inspiradora  atualizada!"
 
 ################################################################################################################
 
-class DoadorDelete(DeleteView):
+class DoadorDelete(SuccessMessageMixin,DeleteView):
     template_name = 'paginasweb/form.html'
     model = Doador
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Excluir Instituicao", "botao" : "Excluir"}
-
-class InstituicaoDelete(DeleteView):
+    success_message = "Doador excluido!"
+class InstituicaoDelete(SuccessMessageMixin,DeleteView):
     template_name = 'paginasweb/form.html'
     model = Instituicao
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Excluir Instituicao", "botao" : "Excluir"}
-
-class StatusDelete(DeleteView):
+    success_message = "Instituição excluida!"
+class StatusDelete(SuccessMessageMixin,DeleteView):
     template_name = 'paginasweb/form.html'
     model = Status
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Excluir Instituicao", "botao" : "Excluir"}
-
-class DoacaoDelete(DeleteView):
+    success_message = "Status excluido!"
+class DoacaoDelete(SuccessMessageMixin,DeleteView):
     template_name = 'paginasweb/form.html'
     model = Doacao
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Excluir Instituicao", "botao" : "Excluir"}
-
-class Historia_InspiradorasDelete(DeleteView):
+    success_message = "Doação excluida!"
+class Historia_InspiradorasDelete(SuccessMessageMixin,DeleteView):
     model = Historia_Inspiradoras
     success_url = reverse_lazy('index')
     extra_context = {'titulo' : "Excluir Instituicao", "botao" : "Excluir"}
-
+    success_message = "Historia Inspiradora excluida!"
 
     ######################################################################################################
 
