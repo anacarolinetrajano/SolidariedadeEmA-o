71c7f96c56e7e6d5f87cc4489eae9761f6d70511
unknown <ana.trajano05@gmail.com>
2025-07-31 12:07:05 -0300

    mudansa do banco para o supabase

--- pw2025/settings.py
+++ pw2025/settings.py
@@ -76,12 +76,23 @@ WSGI_APPLICATION = 'pw2025.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.rwsbmbnmwqxruyrnephc",
+        "PASSWORD": "@Solly12342025",
+        "HOST": "aws-0-sa-east-1.pooler.supabase.com",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
